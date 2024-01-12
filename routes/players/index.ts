ca45aaa89732390374629f6ea4a5bef21c603a68
Simon Hudson <hellosimonhudson@gmail.com>
2024-01-12 15:53:32 +0000

    api tidy up

--- routes/players/index.ts
+++ routes/players/index.ts
@@ -5,55 +5,20 @@ import { sendResponsePayload } from '../../helpers/api';
 
 const COLLECTION_NAME = 'players';
 
-router.get('/', async (req, res) => {
+const makeRequest = async (req, res, query = {}) => {
 	const client = await mongoClient;
 	const db = client.db(process.env.DB_NAME);
-	const response = await db.collection(COLLECTION_NAME).find({}).sort({ last_name: 1 }).toArray();
+	const response = await db.collection(COLLECTION_NAME).find(query).sort({ last_name: 1 }).toArray();
 	sendResponsePayload(response, res);
-});
+};
 
-router.get('/captain', async (req, res) => {
-	const client = await mongoClient;
-	const db = client.db(process.env.DB_NAME);
-	const response = await db.collection(COLLECTION_NAME).find({ is_captain: true }).toArray();
-	sendResponsePayload(response, res);
-});
-
-router.get('/vicecaptain', async (req, res) => {
-	const client = await mongoClient;
-	const db = client.db(process.env.DB_NAME);
-	const response = await db.collection(COLLECTION_NAME).find({ is_vice_captain: true }).toArray();
-	sendResponsePayload(response, res);
-});
-
-router.get('/loan', async (req, res) => {
-	const client = await mongoClient;
-	const db = client.db(process.env.DB_NAME);
-	const response = await db
-		.collection(COLLECTION_NAME)
-		.find({ $or: [{ on_loan_from: { $ne: null } }, { on_loan_to: { $ne: null } }] })
-		.toArray();
-	sendResponsePayload(response, res);
-});
-
-router.get('/loan/in', async (req, res) => {
-	const client = await mongoClient;
-	const db = client.db(process.env.DB_NAME);
-	const response = await db
-		.collection(COLLECTION_NAME)
-		.find({ on_loan_from: { $ne: null } })
-		.toArray();
-	sendResponsePayload(response, res);
-});
-
-router.get('/loan/out', async (req, res) => {
-	const client = await mongoClient;
-	const db = client.db(process.env.DB_NAME);
-	const response = await db
-		.collection(COLLECTION_NAME)
-		.find({ on_loan_to: { $ne: null } })
-		.toArray();
-	sendResponsePayload(response, res);
-});
+router.get('/', async (req, res) => makeRequest(req, res));
+router.get('/captain', async (req, res) => makeRequest(req, res, { is_captain: true }));
+router.get('/vicecaptain', async (req, res) => makeRequest(req, res, { is_vice_captain: true }));
+router.get('/loan', async (req, res) =>
+	makeRequest(req, res, { $or: [{ on_loan_from: { $ne: null } }, { on_loan_to: { $ne: null } }] })
+);
+router.get('/loan/in', async (req, res) => makeRequest(req, res, { on_loan_from: { $ne: null } }));
+router.get('/loan/out', async (req, res) => makeRequest(req, res, { on_loan_to: { $ne: null } }));
 
 export default router;

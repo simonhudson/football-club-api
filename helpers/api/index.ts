0a6cb34097ea69280a344d9874c6a5d77e0fb600
Simon Hudson <hellosimonhudson@gmail.com>
2024-01-13 21:25:49 +0000

    res.send

--- helpers/api/index.ts
+++ helpers/api/index.ts
@@ -16,5 +16,5 @@ export const sendResponsePayload = (response: WithId<any>[], res: Response) => {
 	};
 	res.setHeader('Content-Type', 'application/json');
 	res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-	return res.json(responsePayload);
+	return res.send(responsePayload);
 };

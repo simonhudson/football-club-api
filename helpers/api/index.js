1cad4e952d0e2253ed590a7adb8a1665fd408195
Simon Hudson <hellosimonhudson@gmail.com>
2024-01-14 20:06:57 +0000

    update res.send

--- helpers/api/index.js
+++ helpers/api/index.js
@@ -8,7 +8,7 @@ const sendResponsePayload = (response, res) => {
 	};
 	res.setHeader('Content-Type', 'application/json');
 	res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-	return res.send(responsePayload);
+	res.send(responsePayload);
 };
 
 module.exports = { sendResponsePayload };
--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-// const express = require('express');
-// const players = require('./routes/players');
-
-// const app = express();
-// app.use(express.json());
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => console.log('Server Listening on PORT:', PORT));
-
-// app.use('/players', players);
-
-// module.exports = app;
-
-// index.js
 const express = require('express');
+const players = require('./routes/players');
 
 const app = express();
-const PORT = 4000;
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-	console.log(`API listening on PORT ${PORT} `);
-});
+app.listen(PORT, () => console.log('Server Listening on PORT:', PORT));
 
-app.get('/', (req, res) => {
-	res.send('Hey this is my API running 🥳');
-});
+app.use('/players', players);
 
-app.get('/about', (req, res) => {
-	res.send('This is my about route..... ');
-});
-
-// Export the Express API
 module.exports = app;
+
+// index.js
+// const express = require('express');
+
+// const app = express();
+// const PORT = 4000;
+
+// app.listen(PORT, () => {
+// 	console.log(`API listening on PORT ${PORT} `);
+// });
+
+// app.get('/', (req, res) => {
+// 	res.send('Hey this is my API running 🥳');
+// });
+
+// app.get('/about', (req, res) => {
+// 	res.send('This is my about route..... ');
+// });
+
+// // Export the Express API
+// module.exports = app;

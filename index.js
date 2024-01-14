8a82c9b8c7adffad398fe329da0ec9d9647c6d30
Simon Hudson <hellosimonhudson@gmail.com>
2024-01-14 21:09:53 +0000

    tidy up

--- index.js
+++ index.js
@@ -10,24 +10,3 @@ app.listen(PORT, () => console.log('Server Listening on PORT:', PORT));
 app.use('/players', players);
 
 module.exports = app;
-
-// index.js
-// const express = require('express');
-
-// const app = express();
-// const PORT = 4000;
-
-// app.listen(PORT, () => {
-// 	console.log(`API listening on PORT ${PORT} `);
-// });
-
-// app.get('/', (req, res) => {
-// 	res.send('Hey this is my API running 🥳');
-// });
-
-// app.get('/about', (req, res) => {
-// 	res.send('This is my about route..... ');
-// });
-
-// // Export the Express API
-// module.exports = app;

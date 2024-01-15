3231dbd2fa8c4863162a80c2b1b5fcb11bf1cf05
Simon Hudson <hellosimonhudson@gmail.com>
2024-01-15 07:53:14 +0000

    tidy

--- helpers/api/index.ts
+++ helpers/api/index.ts
@@ -14,7 +14,6 @@ export const sendResponsePayload = (response: WithId<any>[], res: Response) => {
 		metadata: { count: response.length },
 		data: response,
 	};
-	// res.setHeader('Content-Type', 'application/json');
-	// res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+
 	res.json(responsePayload);
 };
--- test/mock-data/category.ts
+++
@@ -1,4 +0,0 @@
-export const MockCategory = {
-	_id: 'id-value',
-	value: 'value-value',
-};
